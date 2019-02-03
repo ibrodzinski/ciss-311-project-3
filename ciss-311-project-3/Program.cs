0f12354fd05202eb52193f07b6d8f208611ca4c9
Eric Smith <eric.smith@solidrockitsolutions.com>
2019-02-02 20:10:19 -0600

    Init Project

---
+++ ciss-311-project-3/Program.cs
@@ -0,0 +1,35 @@
+﻿/// Group Members:
+///     Brodzinski, Isabell
+///     McDonald, Brandon
+///     Morton, Amanda
+///     Roseboro, Charles
+///     Smith, Eric
+/// 
+/// CISS 311
+/// Advanced Agile Development
+/// Saturday, February 2nd, 2019
+/// 
+/// Project Three - Tiny Library
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ciss_311_project_3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
