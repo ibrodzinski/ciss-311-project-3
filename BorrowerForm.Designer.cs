8e6fa981470a06995beaf22e78c903fdf7940111
ibrodzinski <47639917+ibrodzinski@users.noreply.github.com>
2019-02-27 14:41:03 -0600

    Add files via upload

---
+++ BorrowerForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AddBorrowers
+{
+    partial class BorrowerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.facultyButton = new System.Windows.Forms.RadioButton();
+            this.studentButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lastNameBox);
+            this.groupBox1.Controls.Add(this.firstNameBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.facultyButton);
+            this.groupBox1.Controls.Add(this.studentButton);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(57, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 277);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Borrower Info";
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameBox.Location = new System.Drawing.Point(183, 191);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(151, 27);
+            this.lastNameBox.TabIndex = 6;
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameBox.Location = new System.Drawing.Point(183, 145);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(151, 27);
+            this.firstNameBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Name:";
+            // 
+            // facultyButton
+            // 
+            this.facultyButton.AutoSize = true;
+            this.facultyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.facultyButton.Location = new System.Drawing.Point(183, 83);
+            this.facultyButton.Name = "facultyButton";
+            this.facultyButton.Size = new System.Drawing.Size(84, 24);
+            this.facultyButton.TabIndex = 2;
+            this.facultyButton.TabStop = true;
+            this.facultyButton.Text = "Faculty";
+            this.facultyButton.UseVisualStyleBackColor = true;
+            this.facultyButton.CheckedChanged += new System.EventHandler(this.facultyButton_CheckedChanged);
+            // 
+            // studentButton
+            // 
+            this.studentButton.AutoSize = true;
+            this.studentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentButton.Location = new System.Drawing.Point(74, 83);
+            this.studentButton.Name = "studentButton";
+            this.studentButton.Size = new System.Drawing.Size(87, 24);
+            this.studentButton.TabIndex = 1;
+            this.studentButton.TabStop = true;
+            this.studentButton.Text = "Student";
+            this.studentButton.UseVisualStyleBackColor = true;
+            this.studentButton.CheckedChanged += new System.EventHandler(this.studentButton_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select borrower type:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(127, 352);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(96, 27);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "&Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(305, 352);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(96, 27);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "&Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // BorrowerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 418);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BorrowerForm";
+            this.Text = "Add a Borrower";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton facultyButton;
+        private System.Windows.Forms.RadioButton studentButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ BorrowerForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddBorrowers
+{
+    public partial class BorrowerForm : Form
+    {
+        //declare database connection string
+        string connectionString;
+        SqlConnection conn;
+
+        //establish connection 
+        public BorrowerForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["AddBorrowers.Properties.Settings.TinyLibraryConnectionString"].ConnectionString;
+        }
+
+        //declare variable for radio buttons Student and Faculty
+        string borrowerType;
+        // if Student button is checked assign it to borrowerType
+        private void studentButton_CheckedChanged(object sender, EventArgs e)
+        {
+            borrowerType = "Student";
+        }
+        //if Faculty button is checked assign it to borrowerType
+        private void facultyButton_CheckedChanged(object sender, EventArgs e)
+        {
+            borrowerType = "Faculty";
+        }
+
+        //Save button will store info in borrower table in database
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            using (conn = new SqlConnection(connectionString)) using (SqlCommand comd = new SqlCommand("INSERT INTO Membership.borrowers (type, first_name, last_name) " +
+                "VALUES ('"+borrowerType+"', @first_name, @last_name)", conn))
+            {
+                //Assign textbox values to parameters
+                conn.Open();
+                comd.Parameters.AddWithValue("@first_name" , firstNameBox.Text);
+                comd.Parameters.AddWithValue("@last_name" , lastNameBox.Text);
+                comd.ExecuteScalar();
+                MessageBox.Show("Borrower Added.");
+            }
+            
+        }
+
+        //button to clear the form
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNameBox.Clear();
+            lastNameBox.Clear();
+        }
+    }
+}
